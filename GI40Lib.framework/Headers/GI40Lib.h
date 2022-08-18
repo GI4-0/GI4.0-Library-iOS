31959a652c0e8731d18446234bba75b9e6263b1f
Raphael Kuhlmann <raphael.kuhlmann@bertelsmann.de>
2022-08-18 15:25:34 +0200

    Added framework

---
+++ GI40Lib.framework/Headers/GI40Lib.h
@@ -0,0 +1,18 @@
+//
+//  GI40Lib.h
+//  GI40Lib
+//
+//  Created by Hanning, Thomas, NMM-NID on 30.01.22.
+//  Copyright © 2022 Hanning, Thomas, NMM-NM. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GI40Lib.
+FOUNDATION_EXPORT double GI40LibVersionNumber;
+
+//! Project version string for GI40Lib.
+FOUNDATION_EXPORT const unsigned char GI40LibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GI40Lib/PublicHeader.h>
+
